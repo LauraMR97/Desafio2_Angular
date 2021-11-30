79ee9390be9eae289516577d1e8594faba91ada8
Laura <lauramorenoramos97@gmail.com>
2021-11-30 21:11:42 +0100

    Pantalla de login avanzada, falta SASS

---
+++ Wiikinder/src/app/index/component/login/login.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent implements OnInit {
+  public nombre : string;
+  public imagen: string;
+  public titulo: string;
+
+  constructor() {
+    this.nombre='Wiikinder';
+    this.imagen='../assets/index.jpg';
+    this.titulo='Welcome';
+   }
+  ngOnInit(): void {
+  }
+
+}
