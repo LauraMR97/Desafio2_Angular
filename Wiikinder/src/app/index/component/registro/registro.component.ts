40f99e07b91fd848d55fc0b7b627cfd1c6511dc1
Laura <lauramorenoramos97@gmail.com>
2021-12-03 22:23:14 +0100

    arreglos diseño registro

--- Wiikinder/src/app/index/component/registro/registro.component.ts
+++ Wiikinder/src/app/index/component/registro/registro.component.ts
@@ -10,11 +10,13 @@ export class RegistroComponent implements OnInit {
   public nombre : string;
   public imagen: string;
   public titulo: string;
+  public logo: string;
 
   constructor() {
     this.nombre='Wiikinder';
     this.imagen='../assets/registro.jpg';
     this.titulo='Registro';
+    this.logo='../assets/logo.png';
    }
 
   ngOnInit(): void {
