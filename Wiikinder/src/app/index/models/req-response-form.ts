38e9f91204f413595a942bedfd2a3e44316dad5f
Laura <lauramorenoramos97@gmail.com>
2021-12-10 18:55:59 +0100

    formulario preferencias funcional

---
+++ Wiikinder/src/app/index/models/req-response-form.ts
@@ -0,0 +1,11 @@
+export interface FormResponse{
+ deporte: number,
+ arte: number,
+ politica: number,
+ tipoRelaccion: string,
+ numHijos:number,
+ quiereHijos: number,
+ generoPreferido: number,
+ descripcion: string
+ correo: string
+}
