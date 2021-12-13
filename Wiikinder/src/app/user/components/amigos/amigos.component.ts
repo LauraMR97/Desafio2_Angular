e2d7f9a1fda5ed639f6c65c9eabcd8a9dca7ea35
Laura <lauramorenoramos97@gmail.com>
2021-12-13 20:49:50 +0100

    arreglos

--- Wiikinder/src/app/user/components/amigos/amigos.component.ts
+++ Wiikinder/src/app/user/components/amigos/amigos.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import {RestIndexLoginService} from '../../../index/services/rest-index-login.service';
+import { RestUserService } from '../../services/rest-user.service';
 
 @Component({
   selector: 'app-amigos',
@@ -7,16 +10,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AmigosComponent implements OnInit {
 
-
+  public amigos: any = []
   public nombre : string;
   public logo: string;
+  public correo: string;
 
-  constructor() {
+  constructor(
+    private router: Router,
+    private restUserIndexService: RestIndexLoginService,
+    private restUserService: RestUserService,
+  ) {
     this.nombre='Wiikinder';
     this.logo='../assets/logo.png';
+    this.correo='';
    }
 
   ngOnInit(): void {
+    this.getCorreo();
+    this.getAmigos();
   }
 
+  public getCorreo(){
+    this.restUserIndexService.correo.subscribe(correo =>{
+      this.correo=correo;
+    });
+  }
+
+  public getAmigos(){
+    this.restUserService.getAmigosLista(this.correo).subscribe((response)=>{
+        this.amigos=response;
+        this.restUserService.darCorreo(this.correo);
+      });
+    }
+
+  onVolver(){
+    this.router.navigate(['/usuario/menu']);
+  }
+
+  onBorrar(){
+    this.router.navigate(['/usuario/menu']);
+  }
 }
